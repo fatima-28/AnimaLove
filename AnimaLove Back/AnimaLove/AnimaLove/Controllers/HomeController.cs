80099fc75d449190ac0ef20c3098fd9f6dd960e2
fatima-28 <2n6gddb@code.edu.az>
2022-07-20 11:51:46 -0700

    first commit

---
+++ AnimaLove Back/AnimaLove/AnimaLove/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using AnimaLove.DAL;
+using AnimaLove.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AnimaLove.Controllers
+{
+    public class HomeController : Controller
+    {
+        private AppDbContext _context { get; }
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            HomeViewModel home = new HomeViewModel
+            {
+                Slides = _context.Slides.Where(s => !s.IsDeleted).ToList(),
+                SlideSummaries= _context.SlideSummaries.Where(s => !s.IsDeleted).ToList()
+
+            };
+            return View(home);
+        }
+    }
+}
---
+++ AnimaLove Back/AnimaLove/AnimaLove/ViewModels/HomeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using AnimaLove.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AnimaLove.ViewModels
+{
+    public class HomeViewModel
+    {
+        public List<Slide> Slides { get; set; }
+        public List<SlideSummary> SlideSummaries { get; set; }
+    }
+}
