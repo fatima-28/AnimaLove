7b52fb99708cd9ff8456c5a7e090e59f4d8c18bd
fatima-28 <2n6gddb@code.edu.az>
2022-08-27 04:53:44 -0700

    users searcH:

--- AnimaLove Back/AnimaLove/AnimaLove/Controllers/MessageController.cs
+++ AnimaLove Back/AnimaLove/AnimaLove/Controllers/MessageController.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using AnimaLove.DAL;
+using AnimaLove.Models;
+using AnimaLove.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -10,9 +15,34 @@ namespace AnimaLove.Controllers
     [Authorize]
     public class MessageController : Controller
     {
+        private AppDbContext _context { get; }
+       
+        private IWebHostEnvironment _env { get; }
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+
+        public MessageController(AppDbContext context, IWebHostEnvironment env, UserManager<AppUser> userManager,
+                                      SignInManager<AppUser> signInManager,
+                                      RoleManager<IdentityRole> roleManager)
+        {
+            _context = context;
+          
+            _env = env;
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            MessagesViewModel user = new MessagesViewModel
+            {   users=_context.Users.Where(u=>!u.IsActivated).ToList()
+                 
+
+            };
+            return View(user);
         }
     }
 }
